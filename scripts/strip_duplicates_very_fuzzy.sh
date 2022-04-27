2ec621b2a4ea8fd7780c789bb2de6e9942a00407
John D Jones III <jnbek1972@gmail.com>
2022-04-27 18:12:13 -0500

    Update strip_duplicates_very_fuzzy.sh

--- scripts/strip_duplicates_very_fuzzy.sh
+++ scripts/strip_duplicates_very_fuzzy.sh
@@ -1,1 +1,9 @@
-ffmpeg -i video/20220424_205717.MOV -vf mpdecimate=frac='0.55':hi='(64*25)':lo='(64*10)',setpts=N/FRAME_RATE/TB -an -y output/out_2.mp4
+#!/bin/sh
+
+ffmpeg -i video/VID_20220424_210110.mp4 -vf mpdecimate=frac='0.65':hi='(64*45)':lo='(64*15)',setpts=N/FRAME_RATE/TB -an -y output/out_cell.mp4
+
+# Best effort on Night Vids filmed on the Galaxy A50 probably overkill though
+# ffmpeg -i video/VID_20220424_210110.mp4 -vf mpdecimate=frac='0.65':hi='(64*45)':lo='(64*15)',setpts=N/FRAME_RATE/TB -an -y output/out_cell.mp4
+
+# Good for Night Videos filmed with the AKASO
+#ffmpeg -i video/VID_20220424_210110.mp4 -vf mpdecimate=frac='0.51':hi='(64*15)':lo='(64*10)',setpts=N/FRAME_RATE/TB -an -y output/out_cell.mp4
